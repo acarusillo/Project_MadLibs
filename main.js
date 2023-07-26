db0fc2c96b90c7e577888912ef10c74e02cda461
Aki <ancgrouptx@gmail.com>
2023-07-26 15:20:22 -0500

    js is updated

---
+++ main.js
@@ -0,0 +1,11 @@
+const adj1 = document.getElementById("adj1");
+const noun1 = document.getElementById("noun1");
+const adj2 = document.getElementById("adj2");
+const clothing = document.getElementById("clothing");
+const bodyPart = document.getElementById("bodyPart");
+const verb1 = document.getElementById("verb1");
+const food = document.getElementById("food");
+
+arr = [adj1, adj2, noun1, verb1, clothing, bodyPart, food];
+
+console.table(arr);
