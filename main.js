41c11f0ad8773e570d714addb7d9c150143f8e15
Aki <ancgrouptx@gmail.com>
2023-07-27 16:31:52 -0500

    JS Line 20 is added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Variables for user unput
 const adj1 = document.getElementById("adj1");
 const noun1 = document.getElementById("noun1");
 const adj2 = document.getElementById("adj2");
@@ -8,7 +9,7 @@ const food = document.getElementById("food");
 
 // Variable to display the mad lib
 
-const madLibDisplay = document.querySelector("mad-lib");
+const madLibDisplay = document.querySelector(".mad-lib");
 
 //Variable for the form
 
@@ -16,7 +17,8 @@ const form = document.querySelector("form");
 
 //Variable for the mad lib story
 
-const story = "My friend and I started a secret club! We meet every day by the "
+const story = `My friend and I started a secret club! We meet every day by the <b>${adj1}</b><b>${noun1}</b>. Our club has some very special rules. If you want to join, you have to wear a <b>${adj2}</b> <b>${clothing}</b> on your <b>${bodyPart}</b>. You are not allowed to <b>${verb1}</b> during meetings, and you can only eat <b>${food}</b> for your meals. If you are interested in joining, let me know!`;
+
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
